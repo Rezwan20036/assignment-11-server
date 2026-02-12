95b3dda20b9b83ec392f555e1f6ef0fa23955998
Rezwan islam <islamrazwen@gmail.com>
2026-02-13 00:19:01 +0600

    Fixed Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['https://infra-report.netlify.app', 'http://localhost:5173'],
+    origin: ['https://infra-report.netlify.app', 'http://localhost:3000'],
     credentials: true
 }));
 app.use(express.json());
